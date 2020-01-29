d214b39755151462bc929c334597c1eb2ff0f26e
Xavier Perez <xperez007@gmail.com>
2020-01-29 20:09:20 +0000

    Automatically backed up by Learn

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -1,6 +1,14 @@
 >key_for_min_value
 end
 
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+key_for_min_value(ikea)
+# => :chair
+ 
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+key_for_min_value(veggies)
+# => "apple"
+end
 
 
 describe "smallest hash value" do
